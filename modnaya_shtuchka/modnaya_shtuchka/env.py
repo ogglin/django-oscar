16eb0779b10f9fc5b565df7e95faad0f93d3fd39
ares <server.ares@gmail.com>
2020-11-12 21:41:25 +0300

    v0.1.0 update oscar, fix design

---
+++ modnaya_shtuchka/modnaya_shtuchka/env.py
@@ -0,0 +1,14 @@
+DEBUG_STATE = True
+SECRET_KEY = '7ivscx8j+^2*rg8u93wex3xu#fvzc7mq2g$y$8%110+-8t%qz6'
+# HOST = 'localhost'
+# HOST = 'modnayashtychka.ru'
+HOST = '95.217.176.145'
+
+DEFAULT_USER = 'django'
+DEFAULT_NAME = 'django'
+DEFAULT_PASS = '2K3QEX#AEcBhwp'
+DEFAULT_PORT = '5432'
+
+MSH_USER = 'mdsh'
+MSH_NAME = 'mdsh'
+MSH_PASS = '2K3QEX#AEcBhwp'
