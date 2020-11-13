23b227184279ca35050de936336ec0ca3ba07b1b
ares <server.ares@gmail.com>
2020-11-13 18:39:05 +0300

    fix

--- modnaya_shtuchka/modnaya_shtuchka/production.py
+++ modnaya_shtuchka/modnaya_shtuchka/production.py
@@ -2,5 +2,5 @@ from .settings import *
 
 DEBUG = False
 # COMPRESS_OFFLINE = True
-LIBSASS_OUTPUT_STYLE = 'compressed'
+# LIBSASS_OUTPUT_STYLE = 'compressed'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
